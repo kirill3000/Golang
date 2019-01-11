412c81e1f42f69ef327ae28c90a1d3db88b0b6f5
kirill3000 <kiril-2012@list.ru>
2019-01-12 00:21:23 +0300

    test1

---
+++ test.go
@@ -0,0 +1,16 @@
+﻿package main
+
+import (
+    "fmt" // пакет для форматированного ввода вывода
+	"net/http"
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World"))
+	 fmt.Printf("%T\n", "www")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":9000", nil)
+}
